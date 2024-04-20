9a018d06a4e99f9c125664e4f6b4c8d658e1d51b
Justin <justinstoner2@gmail.com>
2024-04-19 18:44:58 -0700

    vite conversion

--- trivia-forge/src/App.jsx
+++ trivia-forge/src/App.jsx
@@ -2,33 +2,39 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/HomePage";
+import Navigation from "./Components/Nav";
+import TriviaGenPage from './Pages/TriviaGenPage';
+import MyTrivia from './Pages/MyTrivia';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <div className="App">
+      <BrowserRouter>
+
+        <header></header>
+        <Navigation />
+        <main>
+
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/triviaGen" element={<TriviaGenPage />} />
+            <Route path="/myTrivia" element={<MyTrivia />} />
+          </Routes>
+
+        </main>
+
+        <footer></footer>
+      </BrowserRouter>
+    </div>
+
   )
 }
 
---
+++ trivia-forge/src/Pages/MyTrivia.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import BootstrapTable from '../Components/BootstrapTable';
+
+function MyTrivia() {
+    return (
+        <>
+            <p>
+                My Trivia Page test
+
+            </p>
+            <BootstrapTable />
+        </>
+    );
+
+}
+export default MyTrivia;
