3faada69fe3b3afe2e262150cd767b6f978a294d
emmanueposu <emmanuep@oregonstate.edu>
2024-05-29 21:27:18 -0700

    Cleaned up files

--- trivia-forge/frontend/src/Components/GameCategories.jsx
+++ trivia-forge/frontend/src/Components/GameCategories.jsx
@@ -1,17 +1,7 @@
-import { React, useState, useEffect } from "react";
-import { getCategories } from "../Services/TF-db_services";
+import { React} from "react";
 
 function GameCategories(game) {
-    //const [categories, setCategories] = useState(null);
     const categories = game.data.categories;
-
-
-
-    // useEffect(() => {
-    //     getCategories(game.data).then(res => {
-    //         setCategories(res);
-    //     });
-    // }, []);
 
     return (
         <>
--- trivia-forge/frontend/src/Components/GameQuestions.jsx
+++ trivia-forge/frontend/src/Components/GameQuestions.jsx
@@ -1,9 +1,6 @@
-import { React, useState, useEffect } from "react";
-import { getCategories, getQuestions } from "../Services/TF-db_services";
+import { React} from "react";
 
 function GameQuestions(game) {
-    //const [categories, setCategories] = useState(null);
-    //const [questions, setQuestions] = useState(null);
     let categories = game?.data.categories || [];
     function questionCount() {
         let count = 0;
@@ -13,33 +10,11 @@ function GameQuestions(game) {
         return count;
     };
 
-    // useEffect(() => {
-    //     getCategories(game.data).then( res => {
-    //         setCategories(res);
-    //     });
-    // }, []);
-
-    // useEffect(() => {
-    //     if (categories) {
-    //         const category_ids = new Set();
-    //         for (let i = 0; i < categories.length; i++) {
-    //             category_ids.add(categories[i].id)
-    //         };
-
-    //         getQuestions(category_ids).then( res => {
-    //             setQuestions(res);
-    //         });
-    //     }
-    // }, [categories]);
-
     return (
         <>
-
             <span>{questionCount()}</span>
-
         </>
     )
 }
 
 export default GameQuestions;
-
